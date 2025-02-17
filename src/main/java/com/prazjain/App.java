edb9ccfffde457801a8d186c7947c8a6eee5caa2
Prashant Jain <praz.jain@gmail.com>
2025-02-17 17:36:26 -0500

    Initial project generation

---
+++ src/main/java/com/prazjain/App.java
@@ -0,0 +1,13 @@
+package com.prazjain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
